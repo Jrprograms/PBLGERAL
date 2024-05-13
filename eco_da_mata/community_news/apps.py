4b85ede39a1fbb54cca9cfe5f0f9f6589fd6ec81
Rafael Henrique <rafael.dev2405@gmail.com>
2024-05-12 21:41:23 -0300

    :sparkles: feat: Criação do app de comunidades e notícias

---
+++ eco_da_mata/community_news/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CommunityNewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'community_news'
---
+++ eco_da_mata/community_news/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+"""Não comecei ainda pq não achei a modelagem dos bancos""" 
+
+class Community (models.Model):
+    pass
+
+class News (models.Model):
+    pass
