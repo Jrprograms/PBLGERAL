d76de96ed4a3c778823a3d05629445dceaeebf50
Rafael Henrique <rafael.dev2405@gmail.com>
2024-05-14 22:41:50 -0300

    :ok_hand: style: reenviando os models ja produzidos

--- eco_da_mata_project/people_app/models.py
+++ eco_da_mata_project/people_app/models.py
@@ -1,2 +1,25 @@
 from django.db import models
 
+CATEGORY_CHOICES = [
+    ('PEOPLE', 'Pessoa'),
+    ('INTERPRISE', 'Empresa'),
+    ]
+
+# Create your models here.
+class Subcategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+class Login(models.Model):
+    user = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.models.EmailField(max_length=254)
+
+class People(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    # icon = models.models.models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
+    category = models.models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    id_people = models.IntegerField(primary_key=True)
+    fk_category = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    fk_login = models.ForeignKey(Login, on_delete=models.CASCADE)
