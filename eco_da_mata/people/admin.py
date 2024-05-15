1d7ab5df01a7fd25cf7373f222e497f2478ae103
kaiq-br <limiere234@gmail.com>
2024-05-14 22:14:49 -0300

    atualizando o admin

--- eco_da_mata/people/admin.py
+++ eco_da_mata/people/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import People, Login, Subcategory
 
 # Register your models here.
+admin.site.register(People)
+admin.site.register(Login)
+admin.site.register(Subcategory)
