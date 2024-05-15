a6574154a984ae230ff469dd3416d5fd2a6a99e4
Rafael Henrique <rafael.dev2405@gmail.com>
2024-05-14 22:22:09 -0300

    :card_file_box: raw: Padronização dos nomes dos projetos e apps

--- eco_da_mata_project/eco_da_mata_project/asgi.py
+++ eco_da_mata_project/eco_da_mata_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for eco_da_mata project.
+ASGI config for eco_da_mata_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eco_da_mata.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eco_da_mata_project.settings')
 
 application = get_asgi_application()
--- eco_da_mata_project/people_app/apps.py
+++ eco_da_mata_project/people_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProjectConfig(AppConfig):
+class PeopleAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'project'
+    name = 'people_app'
