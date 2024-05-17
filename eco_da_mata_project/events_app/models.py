2b630ec644e30b68419ee4bab71d9eaf55fd49a5
Daniel Santos <danielsan171817@gmail.com>
2024-05-17 17:24:45 -0300

    Atualização para teste

--- eco_da_mata_project/events_app/models.py
+++ eco_da_mata_project/events_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#Events an reviews entities:
 
 class Event(models.Model):
+
+    #test
 
     title = models.CharField(max_length=100)
 
@@ -16,19 +18,19 @@ class Event(models.Model):
     adress = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
 
-    link = models.CharField(max_length=100)
+    link = models.URLField()
     value = models.DecimalField(decimal_places=2,max_digits=100)
 
-    pix_key = models.CharField(max_length=500)
+    pix_key = models.URLField(max_length=100)
     pix_key_owner = models.CharField(max_length=100)
     bank_name = models.CharField(max_length=100)
 
-    pdf_link = models.CharField(max_length=300)
-    questionary_link = models.CharField(max_length=200)
+    pdf_link = models.URLField(max_length=100)
+    questionary_link = models.URLField(max_length=100)
     category = models.CharField(max_length=20, choices="")
+    format = models.Choices("Syncron", "Assincron-")
     
     # fk_project = models.ForeignKey(None, on_delete=True)
-    # fk_image = models.ForeignKey(None, on_delete=True)
 
 # class report(models.Model):
 #     pass
