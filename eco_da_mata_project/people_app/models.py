8ca7ef0164cb72f3b08b08ae7015b31baeeb74c4
kaiq-br <limiere234@gmail.com>
2024-05-16 21:05:04 -0300

    poucas coisas

--- eco_da_mata_project/people_app/models.py
+++ eco_da_mata_project/people_app/models.py
@@ -16,7 +16,7 @@ class Subcategory(models.Model):
 class Login(models.Model):
     user = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    email = models.models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254)
 
     def __str__(self):
         return self.name
@@ -25,8 +25,7 @@ class People(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     # icon = models.models.models.ImageField(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
-    category = models.models.CharField(max_length=50, choices=CATEGORY_CHOICES)
-    id_people = models.IntegerField(primary_key=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     fk_category = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
     fk_login = models.ForeignKey(Login, on_delete=models.CASCADE)
 
