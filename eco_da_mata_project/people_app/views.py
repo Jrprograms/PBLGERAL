63f1bcd870a960d5d4954608fb95aa8a3453ab59
kaiq-br <limiere234@gmail.com>
2024-05-18 20:00:10 -0300

    atualização view

--- eco_da_mata_project/people_app/views.py
+++ eco_da_mata_project/people_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import People
 
 # Create your views here.
+
+def getPeople(request):
+    return render(request, 'html?', {'pessoas': People.objects.all})
