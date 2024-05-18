1091e27a84eb9841ac8a192de36e1d94f4fbce01
kaiq-br <limiere234@gmail.com>
2024-05-18 17:08:11 -0300

    arrumação básica

--- eco_da_mata_project/people_app/admin.py
+++ eco_da_mata_project/people_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from people_app.models import People, Login, Subcategory
+from people_app.models import People, Subcategory
 
 # Register your models here.
 admin.site.register(People)
-admin.site.register(Login)
 admin.site.register(Subcategory)
