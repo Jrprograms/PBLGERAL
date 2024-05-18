5a4568f2e7144a02ce2d81a7a2f4dfb576b1b96d
Daniel Santos <danielsan171817@gmail.com>
2024-05-18 00:01:02 -0300

    Atualização model - choices 1

--- eco_da_mata_project/events_app/models.py
+++ eco_da_mata_project/events_app/models.py
@@ -6,29 +6,39 @@ class Event(models.Model):
 
     #test
 
-    title = models.CharField(max_length=100)
-
-    description = models.CharField(max_length=100)
-    profile_image = models.ImageField()
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    profile_image = models.ImageField(name='', width_field=30, height_field=30)
+    #__
 
     start_date = models.DateField()
     end_date = models.DateField()
     time = models.TimeField()
-
     adress = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
+    #__
 
-    link = models.URLField()
-    value = models.DecimalField(decimal_places=2,max_digits=100)
-
+    link = models.URLField(unique=True)
+    value = models.DecimalField(decimal_places=2,max_digits=10)
     pix_key = models.URLField(max_length=100)
     pix_key_owner = models.CharField(max_length=100)
     bank_name = models.CharField(max_length=100)
+    #__
 
     pdf_link = models.URLField(max_length=100)
     questionary_link = models.URLField(max_length=100)
-    category = models.CharField(max_length=20)
-    format = models.CharField(max_length=100)
+    #--
+    category = models.CharField(max_length=20, choices=[
+    (1, "Feira"),
+    (2, "Festividade"),
+    (3, "Celebração")    
+    ])
+    format = models.CharField(max_length=20,choices=[
+    (1, "Presencial"),
+    (2, "Virtual"),
+    (3, "Híbrido")
+    ])
+    #__
     
     # fk_project = models.ForeignKey(None, on_delete=True)
 
