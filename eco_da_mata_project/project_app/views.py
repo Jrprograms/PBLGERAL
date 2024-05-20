a69b3798906324fca1308aa138a8ede39d55cb7c
LuizCosta11 <153194560+LuizCosta11@users.noreply.github.com>
2024-05-20 18:49:41 -0300

    atualização da view de project

--- eco_da_mata_project/project_app/views.py
+++ eco_da_mata_project/project_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
+
+def getallprojects(request):
+    projetos = Project.objects.all()
+    return render(request, 'project.html', {'projetos': projetos})
