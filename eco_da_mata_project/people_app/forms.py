049b96f36f3505dda3b2d0005b06296c51714a66
kaiq-br <limiere234@gmail.com>
2024-05-23 11:43:40 -0300

    Update forms.py

--- eco_da_mata_project/people_app/forms.py
+++ eco_da_mata_project/people_app/forms.py
@@ -1,12 +1,23 @@
 from django import forms 
 from .models import People, Subcategorie
 
-class PeopleForm(forms.ModelForm):
-    class Meta:
-        model = People
-        fields = ['name', 'email', 'description', 'category']
-
 class SubcategoryForm(forms.ModelForm):
     class Meta:
         model = Subcategorie
-        fields = ['nome', 'categoria']
+        fields = ['name', 'category']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'category': forms.Select(attrs={'class':'form-select'})
+            }
+
+class PeopleForm(forms.ModelForm):
+    class Meta:
+        model = People
+        fields = ['name', 'email', 'description', 'category', 'subcategory']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'category': forms.Select(attrs={'class':'form-select'}),
+            'subcategory': forms.Select(attrs={'class':'form-select'}) ,
+            'description': forms.TextInput(attrs={'class':'form-control'}),
+            }
