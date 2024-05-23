2dccb646fda9f98e2ec09bf34128975bf7735dae
kaiq-br <limiere234@gmail.com>
2024-05-22 22:16:55 -0300

    Update urls.py

--- eco_da_mata_project/people_app/urls.py
+++ eco_da_mata_project/people_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('All', views.getAllPeoples, name='Parceiros'),
-    path('All/<str:name>', views.getPeople, name='Adicionar_parceiro')
+    path('list', views.getAllPeoples),
+    path('<str:name>', views.getPeople)
 ]
