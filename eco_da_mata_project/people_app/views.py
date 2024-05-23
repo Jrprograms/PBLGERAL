ba04a09354ef0a05bb381077fc0ac991134fafba
kaiq-br <limiere234@gmail.com>
2024-05-23 11:08:26 -0300

    Update views.py

--- eco_da_mata_project/people_app/views.py
+++ eco_da_mata_project/people_app/views.py
@@ -7,9 +7,10 @@ def getAllPeoples(request):
     pessoas = People.objects.all()
     return render(request, 'index.html', context={'parceiros':pessoas})
 
-def getPeople(request, people_id):
-    parceiro = get_object_or_404(People, people_id)
-    return render(request, 'unique.html', context={'parceiro':parceiro})
+def getPeople(request, people_id): # Corrigir essa bomba
+    parceiro = get_object_or_404(People, pk=people_id)
+    return render(request, 'unique.html', context={'parceiro': parceiro})
+
 
 def createPeople(request):
     pass
