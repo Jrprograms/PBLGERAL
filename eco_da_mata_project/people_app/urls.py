9d3a3c5cb7bc302bb036c9478b2546579a11a00a
kaiq-br <limiere234@gmail.com>
2024-05-24 21:31:15 -0300

    :ok_hand: Alterando o id para as URLs

--- eco_da_mata_project/people_app/urls.py
+++ eco_da_mata_project/people_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('list', views.getAllPeoples),
-    path('<str:name>', views.getPeople),
-    path('<str:name>/delete/', views.deletePeople),
+    path('<int:people_id>', views.getPeople),
+    path('<int:people_id>/delete/', views.deletePeople),
 ]
