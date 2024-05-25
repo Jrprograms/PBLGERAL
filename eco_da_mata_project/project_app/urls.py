801d685f8eb1d783a2ce335cb56a84516a0e8d9d
Luiz <lcferreirac2@gmail.com>
2024-05-25 18:24:15 -0300

    .

--- eco_da_mata_project/project_app/urls.py
+++ eco_da_mata_project/project_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.get_allprojects, name ="projetos"),
     path('<str:Project_ID>/', views.get_project, name='projeto'),
     path('delete/<str:Project_ID>/', views.delete_project, name='Deletando projeto'),
-    #path('create/', views.create_project, name='Criando projeto'),
+    path('create/', views.create_project, name='Criando projeto'),
     path('update/<str:Project_ID>/', views.update_project, name='Editando projeto')
 
 ]
