49f8724fdf298f4bd64cc247fb134a6b508ad38a
Luiz <lcferreirac2@gmail.com>
2024-05-25 14:10:22 -0300

    .

--- eco_da_mata_project/project_app/views.py
+++ eco_da_mata_project/project_app/views.py
@@ -4,19 +4,21 @@ from .models import Project
 
 def get_allprojects(request):
     projetos = Project.objects.all()
-    return render(request, 'project.html', {'Projetos': projetos})
+    return render(request, 'index_project.html', {'Projetos': projetos})
 
 def get_project(request, ID):
      projeto = Project.objects.filter(pk=ID)
-     return render(request, 'project.html', {'Projetos': projeto})
+     return render(request, 'index_project.html', {'Projetos': projeto})
 
 
 def delete_project(request, ID):
-     return render(request, 'delete.html', {'Projetos': projetos})
+     projeto = Project.objects.filter(pk=ID)
+     return render(request, 'delete_project.html', {'Projetos': projeto})
 
-def create_project(request):
-     return render(request, 'project.html', {'Projetos': projetos})
+# def create_project(request):
+#      return render(request, 'create_project.html', {'Projetos': projetos})
 
 
 def update_project(request, ID):
-     return render(request, 'project.html', {'Projetos': projetos})
+     projeto = Project.objects.filter(pk=ID)
+     return render(request, 'update_project.html', {'Projetos': projeto})
