cf16d3ec391a04cc54a2a6138da9ff4463518eef
kaiq-br <limiere234@gmail.com>
2024-05-26 13:54:04 -0300

    :ok_hand: style: adicionando o parâmetro id

--- eco_da_mata_project/people_app/models.py
+++ eco_da_mata_project/people_app/models.py
@@ -10,6 +10,7 @@ CATEGORY_CHOICES = [
 class Subcategorie(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    id = models.IntegerField(auto_created=True, null=False, primary_key=True)
      
     def __str__(self):
         return self.name
