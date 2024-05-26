52abbedb848d9e5fd247268ea66ff25c8a8ea830
kaiq-br <limiere234@gmail.com>
2024-05-25 21:22:33 -0300

    :ok_hand: fix: Concertando id

--- eco_da_mata_project/people_app/models.py
+++ eco_da_mata_project/people_app/models.py
@@ -16,10 +16,11 @@ class Subcategorie(models.Model):
 
 class People(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(null=True, blank=True, max_length=200)
+    email = models.EmailField(null=False, blank=False, max_length=200, default='example@example.com')
     description = models.CharField(max_length=100)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     fk_subcategory = models.ForeignKey(Subcategorie, on_delete=models.CASCADE)
+    id = models.IntegerField(auto_created=True, null=False, blank=False, primary_key=True)
 
     def __str__(self):
         return self.name.username
