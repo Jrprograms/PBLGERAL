ab5741ce581495e4969827d57e9fa9c3ada9beea
kaiq-br <limiere234@gmail.com>
2024-05-25 22:21:41 -0300

    :books: raw: adição da create subcategory as URLs

--- eco_da_mata_project/people_app/urls.py
+++ eco_da_mata_project/people_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:people_id>', views.getPeople),
     path('<int:people_id>/delete/', views.deletePeople),
     path('create/', views.createPeople),
+    path('create/subcategory', views.createSubcategory),
 ]
