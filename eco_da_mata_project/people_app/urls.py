1a087c1f3042499aaa8929e4c883d524d25b5d15
kaiq-br <limiere234@gmail.com>
2024-05-26 22:44:42 -0300

    :bug: fix: adicionando o name da URL

--- eco_da_mata_project/people_app/urls.py
+++ eco_da_mata_project/people_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.getAllPeoples),
     path('<int:people_id>', views.getPeople),
     path('<int:people_id>/delete/', views.deletePeople),
-    path('create/', views.createPeople),
+    path('create/', views.createPeople, name='createPeople'),
     path('create/subcategory', views.createSubcategory),
     path('subcategory/delete/<int:subcategory_id>', views.deleteSubcategory)
 ]
