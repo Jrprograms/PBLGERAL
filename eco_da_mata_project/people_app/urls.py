5ca83ba71814d29c60e32ba830c4322250e0594e
kaiq-br <limiere234@gmail.com>
2024-05-26 23:01:57 -0300

    :ok_hand: raw: criação e alteração de URLs

--- eco_da_mata_project/people_app/urls.py
+++ eco_da_mata_project/people_app/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.getAllPeoples),
-    path('<int:people_id>', views.getPeople),
+    path('<int:people_id>/', views.getPeople),
+    path('<int:people_id>/update/', views.updatePeople),
     path('<int:people_id>/delete/', views.deletePeople),
     path('create/', views.createPeople, name='createPeople'),
     path('create/subcategory', views.createSubcategory),
-    path('subcategory/delete/<int:subcategory_id>', views.deleteSubcategory)
+    path('subcategory/delete/<int:subcategory_id>/', views.deleteSubcategory)
 ]
