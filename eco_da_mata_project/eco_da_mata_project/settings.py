352c862b788e3ef0b30015b8bd037747d5fab7f7
StzDaniel <danielsan171817@gmail.com>
2024-06-03 20:49:52 -0300

    :heavy_plus_sign: feat: criação de rotas da API

--- eco_da_mata_project/eco_da_mata_project/settings.py
+++ eco_da_mata_project/eco_da_mata_project/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
