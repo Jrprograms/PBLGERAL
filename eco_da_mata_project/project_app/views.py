c71fa29a605be8d3839c58fec656314c1bb07a6d
VictoriousSheep <Rivaldobcjunior@gmail.com>
2024-06-03 19:11:52 -0300

    :rotating_light: testing in dev now
    Coloquei o bagulho de update que era minha parte, atrasadaço eu sei e agora nem precisa mais então depois apago

--- eco_da_mata_project/project_app/views.py
+++ eco_da_mata_project/project_app/views.py
@@ -7,24 +7,32 @@ def get_allprojects(request):
     return render(request, 'index_projects.html', context={'Projetos': projetos})
 
 def get_project(request, Project_ID):
-     projeto = get_object_or_404(Project, id=Project_ID)
-     return render(request, 'index_project.html', context={'Projeto': projeto})
+    projeto = get_object_or_404(Project, id=Project_ID)
+    return render(request, 'index_project.html', context={'Projeto': projeto})
 
 
 def delete_project(request, Project_ID):
-     pass
+    pass
 
 def create_project(request):
-     if request.method == 'post':
+    if request.method == 'post':
         form = forms.ProjectForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect(f'')
-     else:
+    else:
         form = forms.ProjectForm()
         contexto = {'form': form}
         return render(request, 'create_project.html', context=contexto)
 
 
 def update_project(request, Project_ID):
-     pass
+    projeto = get_object_or_404(Project, pk=Project_ID)
+    if request.method == 'PUT':
+        form = forms.ProjectForm(request.PUT)
+        if form.is_valid():
+            form.save()
+            return redirect('../../')
+    else:
+        form = forms.ProjectForm()
+    return render(request, 'update_project.html', context={'Projeto': projeto})
