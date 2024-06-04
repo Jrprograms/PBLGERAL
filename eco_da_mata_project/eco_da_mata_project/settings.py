3b9f59f9167e573e4230011f72ec26882fbd0716
kaiq-br <limiere234@gmail.com>
2024-06-04 20:16:00 -0300

    :sparkles: feat: Criação da SECRET_KEY

--- eco_da_mata_project/eco_da_mata_project/settings.py
+++ eco_da_mata_project/eco_da_mata_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,10 +25,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sx6=dikm%o=j=n9cbh4%68pc8-3j6rz$v^a68_c3%=4-xdj9fi'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
