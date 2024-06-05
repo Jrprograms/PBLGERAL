ce3338c9df993aa94d5553688a8402c7d8f8e8d7
StzDaniel <danielsan171817@gmail.com>
2024-06-05 18:38:12 -0300

    :heavy_plus_sign: fix: ajuste na classe e importações de imagem

--- eco_da_mata_project/image_core/models.py
+++ eco_da_mata_project/image_core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from community_app.models import Community, New
+from events_app.models import Event
+from project_app.models import Project
 
-Class Image(models.Model):
+class Image(models.Model):
     
     image = models.ImageField(upload_to = 'images/', null = True)
     category = models.CharField(max_lenght = 50) #incompleta
