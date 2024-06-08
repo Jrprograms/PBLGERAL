ac36fc2b551f2b73c69995a881d1f8cd233d4b1e
kaiq-br <limiere234@gmail.com>
2024-06-08 17:02:43 -0300

    :sparkles: feat: configuração da paginação DRF

--- eco_da_mata_project/eco_da_mata_project/settings.py
+++ eco_da_mata_project/eco_da_mata_project/settings.py
@@ -50,6 +50,11 @@ INSTALLED_APPS = [
     'image_core',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 3
+    }
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
