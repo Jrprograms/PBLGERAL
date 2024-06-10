27e4835d017a8cc6e9c3f37b9e3ec50c8e1d6e3f
Debora Azevedo <azevedodeborah458@gmail.com>
2024-06-09 22:38:01 -0300

    ✨feat: registrando Community no Core.admin

--- eco_da_mata_project/core/admin.py
+++ eco_da_mata_project/core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Imagem
+from community_app.models import Community
+from community_app.models import News
+from events_app.models import Event 
+from people_app.models import People
+from project_app.models import Project 
 
 # Register your models here.
+
+class Imagemnline(admin.TabularInline):
+    model = Imagem
+    extra = 1
+
+class CommunityAdmin(admin.ModelAdmin):
+    inlines = [ImagemInline]
+
+class NewAdmin(admin.ModelAdmin):
+    inlines = [ImagemInline]
+
+
+admin.site.register(Community, CommunityAdmin)
+admin.site.register(New, NewAdmin)
+
