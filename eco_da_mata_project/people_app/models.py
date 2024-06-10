984337623281223b902ac4ac7ed423ae88d278f8
kaiq-br <limiere234@gmail.com>
2024-06-10 20:16:21 -0300

    :bug: fix: id não será mais pedido

--- eco_da_mata_project/people_app/models.py
+++ eco_da_mata_project/people_app/models.py
@@ -10,7 +10,7 @@ CATEGORY_CHOICES = [
 class Subcategorie(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
-    id = models.IntegerField(auto_created=True, null=False, primary_key=True)
+    id = models.AutoField(primary_key=True)
      
     def __str__(self):
         return self.name
@@ -21,7 +21,7 @@ class People(models.Model):
     description = models.CharField(max_length=100)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     fk_subcategory = models.ForeignKey(Subcategorie, on_delete=models.CASCADE)
-    id = models.IntegerField(auto_created=True, null=False, blank=False, primary_key=True)
+    id = models.AutoField(primary_key=True)
     photo = models.ImageField(upload_to='photos/', null=True)
     link = models.URLField(max_length=200, null=True)
 
