18d65ebc5070f9e1afa09cc05274844723178a70
kaiq-br <limiere234@gmail.com>
2024-06-11 20:47:08 -0300

    :boom: remove: ajeitando links nas urls

--- eco_da_mata_project/eco_da_mata_project/urls.py
+++ eco_da_mata_project/eco_da_mata_project/urls.py
@@ -41,9 +41,6 @@ urlpatterns = [
     path('community/', include('community_app.urls')), 
     path('project/', include('project_app.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
     # path('admin/', admin.site.urls),
